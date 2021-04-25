7641d388b06b5ac75d1349b99c412fca3f598cf6
Ozymandias-9 <galac.9173@gmail.com>
2021-04-24 21:23:40 -0500

    saving process

--- files/UI/js/db.js
+++ files/UI/js/db.js
@@ -12,7 +12,6 @@ addEventListener('load', () => {
         axios.get('http://localhost:3000/students/')
         .then((res) => {
             const students = res.data.message;
-            console.log(students);
             fragment = document.createDocumentFragment();
             for(let els of students){
                 const element = document.createElement('DIV');
@@ -25,5 +24,45 @@ addEventListener('load', () => {
             console.log(error);
         })
     }
+
+    document.getElementById('btn-edit').addEventListener('click', ()=>{
+        stud = document.getElementById('input-edit').value
+    
+        axios.get(`http://localhost:3000/students/${stud}`)
+        .then((res)=>{
+            const students = res.data.message;
+            document.getElementById('input_name').value = students[0].nombre;
+            document.getElementById('input_last_name').value = students[0].apellido;
+            document.getElementById('input_number').value = students[0].expediente;
+            document.getElementById('input_career').value = students[0].carrera;
+        }).catch((err)=>{
+            console.log(err);
+        })
+    })
+
+    document.getElementById('btn-fin-edit').addEventListener('click', ()=>{
+        stud = document.getElementById('input-edit').value
+        axios({
+            method: 'put',
+            url: `http://localhost:3000/students/${stud}`,
+            data:{
+                nombre: document.getElementById('input_name').value,
+                apellido: document.getElementById('input_last_name').value,
+                expediente: document.getElementById('input_number').value,
+                carrera: document.getElementById('input_career').value
+            }
+        }).then((res)=>{
+            alert("Estudiante editado correctamente.");
+            const juas = base.childNodes.length;
+            for(let i = 0; i<juas; i++){
+                base.removeChild(base.lastChild);
+            }
+            loadStudents();
+        }).catch((err)=>{
+            console.log(err);
+        })
+    })
 })
+
+
 
