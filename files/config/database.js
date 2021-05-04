d28ec1e35023099a0c708bf06d7f17e5912cb0a2
Ozymandias-9 <galac.9173@gmail.com>
2021-05-04 10:42:28 -0500

    make it better

--- files/config/database.js
+++ files/config/database.js
@@ -2,11 +2,11 @@ const mysql = require ('mysql');
 const util = require ('util');
 
 const pool = mysql.createPool({
-    connectionLimit: 1000,
-    host: 'databases.000webhost.com',
-    user: 'id16683812_dicias',
-    password: 'J3\Sj~fcu4No<Wnj',
-    database: 'id16683812_achu123',
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'node',
 });
 
 
