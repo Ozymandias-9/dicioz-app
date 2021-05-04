fac151e044a76e18e58f1cfad8faa898fc6d8aa3
Ozymandias-9 <galac.9173@gmail.com>
2021-05-03 23:16:02 -0500

    ready for heroku

--- files/config/database.js
+++ files/config/database.js
@@ -2,12 +2,14 @@ const mysql = require ('mysql');
 const util = require ('util');
 
 const pool = mysql.createPool({
-    connectionLimit: 10,
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'node'
+    connectionLimit: 1000,
+    host: 'databases.000webhost.com',
+    user: 'id16683812_dicias',
+    password: 'J3\Sj~fcu4No<Wnj',
+    database: 'id16683812_achu123',
 });
 
+
 pool.query = util.promisify(pool.query);
+
 module.exports = pool; 
